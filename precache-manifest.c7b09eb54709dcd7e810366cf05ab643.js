3fcd4dccfd2a9f1985557fe169d3aeaf1daf5b23
Rajesh mule <rajeshsmule@gmail.com>
2020-03-11 10:45:14 +0530

    Updates

---
+++ precache-manifest.c7b09eb54709dcd7e810366cf05ab643.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21305b2e15ae4ef7d95a6b72689da173",
+    "url": "/tic-tac-to/index.html"
+  },
+  {
+    "revision": "dbe227c21f79c1082935",
+    "url": "/tic-tac-to/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1367132b787ced0226f2",
+    "url": "/tic-tac-to/static/js/2.46955d1d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tic-tac-to/static/js/2.46955d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe227c21f79c1082935",
+    "url": "/tic-tac-to/static/js/main.92cdca01.chunk.js"
+  },
+  {
+    "revision": "8b791338dadc17453eb2",
+    "url": "/tic-tac-to/static/js/runtime-main.7d11c967.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-to/static/media/logo.5d5d9eef.svg"
+  }
+]);
