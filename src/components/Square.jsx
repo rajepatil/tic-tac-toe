8da169c5f05353def9dc91276be05e6db3714651
Rajesh mule <rajeshsmule@gmail.com>
2020-03-12 03:32:54 +0530

    complet the game

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={function() { alert('click'); }}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
